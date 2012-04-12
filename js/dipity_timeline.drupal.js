ee0cc813ca8a822bec11d7f6fec73d26052f0652
Stefan Auditor <stefan.auditor@erdfisch.de>
2012-04-12 14:27:00 +0200

    removing dbg statements

--- js/dipity_timeline.drupal.js
+++ js/dipity_timeline.drupal.js
@@ -2,8 +2,6 @@
   Drupal.behaviors.dipityTimeline = {
     attach: function (context, settings) {
       $.each(settings.dipity_timeline, function(index, value) { 
-        console.log(index);
-        console.log(value);
         var canvas = document.getElementById('dipity-timeline-dom-id-' + index);
         var timeline = new dipity.widget(canvas, value);
       });
